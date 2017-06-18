95f7ad8951838209d5be3bca914d0c97277247d6
Shaun <shaun.b.thompson@gmail.com>
2017-06-18 14:43:48 -0400

    fun with functions

--- alarmcenter.py
+++ alarmcenter.py
@@ -13,11 +13,11 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.uix.label import Label
-# from kivy.uix.button import Button
+from kivy.uix.button import Button
 # from kivy.uix.togglebutton import ToggleButton
 # from kivy.uix.floatlayout import FloatLayout
 # from kivy.uix.gridlayout import GridLayout
-# from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.boxlayout import BoxLayout
 # from kivy.config import Config
 # from kivy.clock import Clock
 # from kivy.properties import StringProperty, NumericProperty, ObjectProperty, BooleanProperty
@@ -33,8 +33,35 @@ from kivy.uix.label import Label
 
 # main
 class AlarmCenterApp(App):
+    def build_time_tile(self):
+        # build time tile
+        tile = Button(text='HH:MM::ss')
+        return tile
+
+    def build_date_tile(self):
+        tile = Button(text='dd MMM YYYY')
+        return tile
+
     def build(self):
-        return Label(text="Hello from AlarmCenter")
+        # create root layout
+        root = BoxLayout(orientation='horizontal')
+
+        # create left column
+        left = BoxLayout(orientation='vertical')
+
+        left.add_widget(self.build_time_tile())
+        left.add_widget(self.build_date_tile())
+
+        right = BoxLayout(orientation='vertical')
+        alarm1 = Button(text='HH::MM')
+        alarm2 = Button(text='HH::MM')
+        right.add_widget(alarm1)
+        right.add_widget(alarm2)
+
+        root.add_widget(left)
+        root.add_widget(right)
+
+        return root
 
 if __name__ == '__main__':
     AlarmCenterApp().run()
